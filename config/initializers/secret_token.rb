485af2d39ea88eccc13752f00beb7d0a0a638252
Paul Nikitochkin <pftg@jetthoughts.com>
2011-12-11 21:11:08 +0200

    Init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UriMonitor::Application.config.secret_token = 'a74a1c8162c6bed6f3b3761dda8ec4a87802cce055e4a7e79db8360792c56c3dfe670c956372fbe00a29e0af204e1d01922679600ed387f241761afc4c755c60'
